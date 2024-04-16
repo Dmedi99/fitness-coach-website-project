05da5e1e060ce234fb6c8a50bcf2893166869ca6
Dmedi99 <davidmedina0907@gmail.com>
2024-04-16 15:04:35 -0700

    changed some stlying components and added a Services page'

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,26 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import Home from './pages/Home';
 import About from './pages/About';
-import Contact from './pages/Contact'; 
+import Contact from './pages/Contact';
+import Services from './pages/Services';
+import logo from './assets/logo.png';
+
 
 const App = () => {
   return (
     <>
-    <Router>
-      <NavBar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-    </Router>
+      <div className="logo-container">
+        <img src={logo} alt="Logo" className="logo" />
+      </div>
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path='/services' element={<Services />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </Router>
     </>
   );
 };
---
+++ src/pages/Services.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+function Services(){
+    return(
+        <div>
+        <div>
+          <h2 className='title'>Services</h2>
+          <p className='paragraph'>These are the services we provide at Steady State Fitness</p>
+        </div>
+      </div>
+    )
+}
+
+export default Services;
