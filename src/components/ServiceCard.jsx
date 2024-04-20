45c04f89436c632c365155a407e4ea3218017fff
Dmedi99 <davidmedina0907@gmail.com>
2024-04-20 00:59:03 -0700

    added styling to the cards that display the services. When the card is flipped it will show the desciption of the service

--- src/components/ServiceCard.jsx
+++ src/components/ServiceCard.jsx
@@ -1,24 +1,25 @@
-import React from 'react';
+import React from "react";
+import { useState } from "react";
 
 
 function ServiceCard({ name, description }) {
-    const [isFlipped, setIsFlipped] = useState(false);
-  
-    const handleClick = () => {
-      setIsFlipped(!isFlipped);
-    };
-  
-    return (
-      <div className={`service-card ${isFlipped ? 'flipped' : ''}`} onClick={handleClick}>
-        <div className='front'>
-          <h3>{name}</h3>
-        </div>
-        <div className='back'>
-          <p>{description}</p>
-        </div>
+  const [isFlipped, setIsFlipped] = useState(false);
+
+  const handleClick = () => {
+    setIsFlipped(!isFlipped);
+  };
+
+  return (
+    <div className={`service-card ${isFlipped ? 'flipped' : ''}`} onClick={handleClick}>
+      <div className='front'>
+        <h3>{name}</h3>
       </div>
-    );
-  }
+      <div className='back'>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+}
 
 
   export default ServiceCard;
--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,15 +1,27 @@
 import React from "react";
+import ServiceCard from "../components/ServiceCard";
 
 
 function Services(){
-    return(
-        <div className='page'>
-        <div>
-          <h2 className='title'>Services</h2>
-          <p className='paragraph'>These are the services we provide at Steady State Fitness</p>
+  const services = [
+    { name: 'Service 1', description: 'Description for Service 1' },
+    { name: 'Service 2', description: 'Description for Service 2' },
+    { name: 'Service 3', description: 'Description for Service 3' },
+  ];
+
+  return (
+    <div className='page'>
+      <div>
+        <h2 className='title'>Services</h2>
+        <p className='paragraph'>These are the services we provide at Steady State Fitness</p>
+        <div className='services-list'>
+          {services.map(service => (
+            <ServiceCard key={service.name} name={service.name} description={service.description} />
+          ))}
         </div>
       </div>
-    )
+    </div>
+  );
 }
 
 export default Services;
