6231cc2ed9fb2204ef157460bc6c78256f14a24a
Dmedi99 <davidmedina0907@gmail.com>
2024-04-20 00:14:34 -0700

    Created a ServiceCard component to create flippable cards for the Services Page

---
+++ src/components/ServiceCard.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+
+function ServiceCard({ name, description }) {
+    const [isFlipped, setIsFlipped] = useState(false);
+  
+    const handleClick = () => {
+      setIsFlipped(!isFlipped);
+    };
+  
+    return (
+      <div className={`service-card ${isFlipped ? 'flipped' : ''}`} onClick={handleClick}>
+        <div className='front'>
+          <h3>{name}</h3>
+        </div>
+        <div className='back'>
+          <p>{description}</p>
+        </div>
+      </div>
+    );
+  }
+
+
+  export default ServiceCard;
