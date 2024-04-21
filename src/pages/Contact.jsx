8e833a98f5694a99f0b843f89df7cb9293109951
Dmedi99 <davidmedina0907@gmail.com>
2024-04-21 00:57:32 -0700

    Added feature to Contact.jsx page for the user to send an email to the trainer directly

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,14 +1,30 @@
 // src/pages/Contact.js
 import React from 'react';
+import emailjs from 'emailjs-com';
 
 const Contact = () => {
+  function sendEmail(e) {
+    e.preventDefault();
+
+    emailjs.sendForm('service_rh8yjq1', 'template_15d1zlt', e.target, 'vQjeiBpAw9fEbSx_k')
+      .then((result) => {
+        console.log(result.text);
+      }, (error) => {
+        console.log(error.text);
+      })
+  }
   return (
-    <div className='page'>
-      <div>
-        <h2 className='title'>Contact Page</h2>
-        <p className='paragraph'>Let's connect to get your journey started.</p>
-      </div>
-    </div>
+<div className='page'>
+  <div>
+    <h2 className='title'>Contact Page</h2>
+    <p className='paragraph'>Let's connect to get your journey started.</p>
+    <h3 className='form-title'>Message:</h3>
+    <form className='contact-form' onSubmit={sendEmail}>
+      <textarea className='form-text-area' name="message" />
+      <button className='send-email-button' type="submit">Send</button>
+    </form>
+  </div>
+</div>
   );
 };
 
