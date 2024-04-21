6843e54e423d79e15f9f6403a160b215b8e404e6
Dmedi99 <davidmedina0907@gmail.com>
2024-04-21 12:11:29 -0700

    added a Footer component to add to the bottom of the website and changed the size of the Service cards

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+
+function Footer() {
+    return (
+        <footer className="footer">
+            <h1 className="footer header">STEADY STATE FITNESS</h1>
+            <p className="copyright"> © Copyright 2024</p>
+            <Link to='/contact'>Contact Us</Link>
+        </footer>
+    )
+}
+
+export default Footer;
