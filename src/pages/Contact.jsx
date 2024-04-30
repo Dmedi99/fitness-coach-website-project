54caf963b3dca32b44170cff582400312500e172
Dmedi99 <davidmedina0907@gmail.com>
2024-04-30 15:50:12 -0700

    updated message form and the styling for it

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -20,8 +20,31 @@ const Contact = () => {
     <p className='paragraph'>Let's connect to get your journey started.</p>
     <h3 className='form-title'>Message:</h3>
     <form className='contact-form' onSubmit={sendEmail}>
-      <textarea className='form-text-area' name="message" />
-      <button className='send-email-button' type="submit">Send</button>
+      <label>
+        Name:
+        <input type='text' name='user_name' placeholder='Name' required />
+      </label>
+      <label>
+        Email:
+        <input type='email' name='user_email' placeholder='Email' required />
+      </label>
+      <label>
+        Phoner Number:
+        <input type='text' name='contact_number' placeholder='(Optional)'/>
+      </label>
+      <label>
+        Best way to reach you: 
+        <select name='contact_preference' placeholder='select one'>
+          <option value='email'>Email</option>
+          <option value='phone'>Phone</option>
+          <option value='text'>Text</option>
+        </select>
+      </label>
+      <label>
+        Message:
+        <textarea className='form-text-area' name='message' placeholder="Let's connect" required />
+      </label>
+      <button className='send-email-button' type='submit'>Send</button>
     </form>
   </div>
 </div>
