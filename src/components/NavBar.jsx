0a798794ebb005685c957ac8bb358d908c2ef96a
Dmedi99 <davidmedina0907@gmail.com>
2024-07-18 14:31:00 -0700

    updated Navbar and fixed issue where the burger menu button was not showing in smaller screen sizes. More updates to design comming soon.

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,37 +1,39 @@
-// src/components/NavBar.jsx
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import logo from '../assets/logo.png';
-import './NavBar.css';
+import './NavBar.css'; // Ensure you have the CSS in the same directory or update the path accordingly
+import logo from '../assets/logo.png'
 
-function NavBar() {
-  const [navActive, setNavActive] = useState(false);
+const NavBar = () => {
+    const [navActive, setNavActive] = useState(false);
 
-  const handleClick = () => {
-    setNavActive(!navActive);
-  }
+    const handleBurgerClick = () => {
+        setNavActive(!navActive);
+    };
 
-  return (
-    <nav>
-      <div className='logo-container'>
-        <Link to="/">
-          <img src={logo} alt="Logo" className="logo" />
-        </Link>
-      </div>
-      <ul className={`nav-links ${navActive ? 'nav-active' : ''}`}>
-        {['Home', 'About', 'Services', 'Contact'].map((text, index) => (
-          <li key={index} style={{ animation: navActive ? `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s` : '' }}>
-            <Link to={`/${text.toLowerCase()}`}>{text}</Link>
-          </li>
-        ))}
-      </ul>
-      <div className={`burger ${navActive ? 'toggle' : ''}`} onClick={handleClick}>
-        <div className='line1'></div>
-        <div className='line2'></div>
-        <div className='line3'></div>
-      </div>
-    </nav>
-  );
-}
+    return (
+        <nav>
+            <div className="logo-container">
+              <Link to='/'>
+                <img src={logo} alt='logo' className='logo'></img>
+              </Link>
+            </div>
+            <ul className={`nav-links ${navActive ? 'nav-active' : ''}`}>
+                {['Home', 'About', 'Services', 'Contact'].map((text, index) => (
+                    <li key={index} style={{ animation: navActive ? `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s` : '' }}>
+                        <Link to={`/${text.toLowerCase()}`}>{text}</Link>
+                    </li>
+                ))}
+                <li className="button" style={{ animation: navActive ? `navLinkFade 0.5s ease forwards ${4 / 7 + 0.5}s` : '' }}>
+                    <Link to="/consultation">Free Consultation</Link>
+                </li>
+            </ul>
+            <div className={`burger ${navActive ? 'toggle' : ''}`} onClick={handleBurgerClick}>
+                <div className="line1"></div>
+                <div className="line2"></div>
+                <div className="line3"></div>
+            </div>
+        </nav>
+    );
+};
 
 export default NavBar;
